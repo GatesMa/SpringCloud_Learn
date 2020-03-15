10c6e2e634def93e21a16b7311d5b687925c109f
GatesMa <gatesma@foxmail.com>
2020-03-15 21:53:18 +0800

    eureka

--- cloud2020/cloud-provider-payment8002/src/main/java/com/gatesma/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8002/src/main/java/com/gatesma/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.gatesma.springcloud.entities.CommonResult;
 import com.gatesma.springcloud.entities.Payment;
 import com.gatesma.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -23,6 +24,9 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping(value = "payment/hello")
     public String hello() {
         return "hello payment8001";
@@ -33,7 +37,7 @@ public class PaymentController {
         int result = paymentService.create(payment);
         log.info("----- 插入结果Payment： " + result);
         if(result > 0) {
-            return new CommonResult(200, "插入数据库成功", result);
+            return new CommonResult(200, "插入数据库成功, serverPort: " + serverPort, result);
         } else {
             return new CommonResult(444, "插入数据库失败", null);
         }
@@ -44,7 +48,7 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("----- 通过ID获取Payment： " + payment);
         if(payment != null) {
-            return new CommonResult(200, "查询数据库成功", payment);
+            return new CommonResult(200, "查询数据库成功, serverPort: " + serverPort, payment);
         } else {
             return new CommonResult(444, "查询数据库失败， 没有对应ID： " + id, null);
         }
