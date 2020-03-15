393e02c290c3796a4086be8711b8ce9400d71670
GatesMa <gatesma@foxmail.com>
2020-03-15 12:56:07 +0800

    2020最新SpringCloud学习过程

---
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/gatesma/springcloud/PaymentMain8001.java
@@ -0,0 +1,21 @@
+package com.gatesma.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright (C), 2020
+ * FileName: PaymentMain8001
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/3/15 12:52
+ * Description: payment service 8001 服务
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
