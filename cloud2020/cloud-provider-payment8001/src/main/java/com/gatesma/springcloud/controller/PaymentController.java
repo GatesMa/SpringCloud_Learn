c570c9be8ce775561b7a65bbae7c6554f0c1c4c2
GatesMa <gatesma@foxmail.com>
2020-03-15 14:56:50 +0800

    2020最新SpringCloud学习过程

--- cloud2020/cloud-provider-payment8001/src/main/java/com/gatesma/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/gatesma/springcloud/controller/PaymentController.java
@@ -4,6 +4,9 @@ import com.gatesma.springcloud.entities.CommonResult;
 import com.gatesma.springcloud.entities.Payment;
 import com.gatesma.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -23,6 +26,12 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @GetMapping(value = "payment/hello")
+    public String hello() {
+        return "hello payment8001";
+    }
+
+    @PostMapping(value = "/payment/create")
     public CommonResult create(Payment payment) {
         int result = paymentService.create(payment);
         log.info("----- 插入结果Payment： " + result);
@@ -30,6 +39,17 @@ public class PaymentController {
             return new CommonResult(200, "插入数据库成功", result);
         } else {
             return new CommonResult(444, "插入数据库失败", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("----- 获取Payment： " + payment);
+        if(payment != null) {
+            return new CommonResult(200, "查询数据库成功", payment);
+        } else {
+            return new CommonResult(444, "查询数据库失败， 没有对应ID： " + id, null);
         }
     }
 
