9081d8ef5b0a2cc701ff314798b426aa63803aa8
GatesMa <gatesma@foxmail.com>
2020-03-15 20:47:59 +0800

    eureka

--- cloud2020/cloud-eureka-server7002/src/main/java/com/gatesma/springcloud/EurekaMain7002.java
+++ cloud2020/cloud-eureka-server7002/src/main/java/com/gatesma/springcloud/EurekaMain7002.java
@@ -2,22 +2,20 @@ package com.gatesma.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Copyright (C), 2020
- * FileName: OrderMain80
+ * FileName: EurekaApplication7002
  * Author:   Marlon
  * Email: gatesma@foxmail.com
- * Date:     2020/3/15 17:51
+ * Date:     2020/3/15 20:24
  * Description:
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
-
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
-
 }
