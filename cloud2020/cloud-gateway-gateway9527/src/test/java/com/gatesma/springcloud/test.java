3932fc42af9bd44bd71e0d64ef2460c240f0d042
GatesMa <gatesma@foxmail.com>
2020-03-18 11:25:01 +0800

    Gateway

---
+++ cloud2020/cloud-gateway-gateway9527/src/test/java/com/gatesma/springcloud/test.java
@@ -0,0 +1,20 @@
+package com.gatesma.springcloud;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Copyright (C), 2020
+ * FileName: test
+ * Author:   Marlon
+ * Email: gatesma@foxmail.com
+ * Date:     2020/3/18 10:19
+ * Description:
+ */
+public class test {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+
+}
